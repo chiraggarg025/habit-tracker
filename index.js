ca4927ef0baf61d8cde0ff34d1895c5cb5f50b8e
chanakya.garg <chiraggarg025@gmail.com>
2020-07-23 22:30:22 +0530

    add layout and partials

--- index.js
+++ index.js
@@ -2,12 +2,22 @@
 const express = require('express');
 const app = express();
 const port= 8000;
+const expressLayout = require('express-ejs-layouts');
+// set view engine
+app.set('view engine','ejs');
+app.set('views','./views');
+// extract style and scripts from subpages to layout
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+//Use router
+app.use(express.static('./assets'));
+app.use(expressLayout);
 // using router
 app.use('/',require('./routes/index'));
 
-// setting up view engine
-app.set('view engine','ejs');
-app.set('views','./views');
+// require mongoose
+// const db = require('./config/mongoose');
+
 
 
 
